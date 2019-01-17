6a50409c3fb46035a5df9dcbafd9d0e75b30fa6a
Younger <aclrioe@hotmail.com>
2019-01-17 17:36:48 +0800

    修复build后首页空白

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import {increment, decrement, reset} from 'actions/counter';
 import http from '@/api/http';
 import Api from '@/api/api';
 import { connect } from 'react-redux';
+import './Home.css';
 
 class Home extends Component {
     constructor () {
@@ -22,14 +23,14 @@ class Home extends Component {
         const { counter,increment,decrement,reset} = this.props
         console.log(this.props);
         return (
-            <div>
+            <div className="wrap" >
                 this is home <br/>
                 当前计数： { counter.count }
                 <button onClick={ () => increment()} >自增</button>
                 <button onClick={ () => decrement()} >自减</button>
                 <button onClick={ () => reset()} >重置</button>
                 <div>
-                    <a href="/Personal">跳转个人中心</a>
+                    <a href="#/Personal">跳转个人中心</a>
                 </div>
 
                 <button onClick = { () =>  this._getUser() } >获取个人信息</button>
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { HashRouter, Route, Switch, Link } from "react-router-dom";
 import Loadable from './loadable';
 
 const Home= Loadable( () => import('../pages/Home/Home'));
 const Personal = Loadable ( () => import('../pages/Personal/Personal'));
 
 const Router = () => (
-    <BrowserRouter>
+    <HashRouter>
         <Switch>
             <Route exact path="/" component={Home} ></Route>
             <Route exact path="/Personal" component={Personal} ></Route>
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 )
 export default Router;
